e0bfc9a2f2c134329120b2ef163ae166aad1ca5c
TahaRFarouk <tlab24662@gmail.com>
2024-11-24 17:01:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function sendMail(){
+  let parms = {
+      name : document.getElementById("name").value ,
+      email : document.getElementById("email").value ,
+      message : document.getElementById("message").value ,
+      subject :document.getElementById("subject").value
+
+  }
+  emailjs.send("service_rl6gal5","template_4aka1kb",parms).then(alert("email sent"))
+}
