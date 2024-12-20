d081ba2c3239d60adf65e73971e39e60b4d733f1
TahaRFarouk <tlab24662@gmail.com>
2024-12-20 16:40:56 +0100

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php
+
+    $con = mysqli_connect('localhost','root');
+    if ($con){
+        echo "Connection Succefult";
+    }else {
+        echo "Connection Faild";
+    }
+    mysqli_select_db($con,'contactport');
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $number=$_POST['number'];
+    $subject=$_POST['subject'];
+    $message=$_POST['message'];
+    
+    $query ="INSERT INTO users (name,email,number,subject,message) VALUES('$name','$email','$number','$subject','$message')";
+    mysqli_query($con,$query);
+    header('location:index.php'); 
+?>
